74c6689bf6facae3f3ca83d5a49654fd7603d935
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-09-02 23:34:41 +0200

    Google maps api

--- js/script.js
+++ js/script.js
@@ -1,45 +1,36 @@
-// 'use strict';
-// // $('.main-carousel').flickity({
-// //     // options
-// //     cellAlign: 'left',
-// //     contain: true
-// // });
+'use strict';
 
-
-// // external js: flickity.pkgd.js
-
-// var $carousel = $('.carousel').flickity({
-//   imagesLoaded: true,
-//   percentPosition: false,
-//   //disable dots
-//   pageDots: false,
-//   fade: true,
-//   //after refreshing last img
-//   hash: true,
-// });
-// //Select first one
-// document.getElementById("restart").addEventListener("click", function(){
-// $carousel.flickity( 'select', 0, false, true );
-// });
+var $carousel = $('.carousel').flickity({
+  imagesLoaded: true,
+  percentPosition: false,
+  //disable dots
+  pageDots: false,
+  fade: true,
+  //after refreshing last img
+  hash: true,
+});
+//Select first one
+document.getElementById("restart").addEventListener("click", function () {
+  $carousel.flickity('select', 0, false, true);
+});
 
 
 
 ///     Google maps
+var infos = document.getElementById('infos');
+
 // Initialize and add the map
-window.initMap = function() {
+window.initMap = function () {
   // The location of Uluru
-  var warsaw = {lat: 52.237049, lng: 21.017532};
-  var germany= {lat:53.488415,lng:9.142274};
-  var uk ={lat:51.456600,lng:0.105600};
+  var warsaw = { lat: 52.237049, lng: 21.017532 };
+  var germany = { lat: 53.488415, lng: 9.142274 };
+  var uk = { lat: 51.456600, lng: 0.105600 };
   // The map, centered at Warsaw
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: warsaw});
+    document.getElementById('map'), { zoom: 10, center:productsData[0]["coords"]});
   // The marker, positioned at Uluru
-  var markerOne = new google.maps.Marker({position: warsaw, map: map});
-
-
-  markerOne.addListener('click',function(){
-    infos.innerHTML = 'clicked on Poland';
-  });
-
+	for(var i = 0; i < productsData.length; i++){
+    const childArray = productsData[i]["coords"];
+    var marker = new google.maps.Marker({ position: childArray, map: map });
+	}
 }
